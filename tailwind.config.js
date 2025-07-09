da27ce3ef06e3cf22da92f9c5302c378c9bad0d1
vishalskv <vishal.skv.21@gmail.com>
2025-07-09 15:50:28 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  darkMode: "class", // or 'media' if you want system preference
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
